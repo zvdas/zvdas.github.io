872ced111bfd47c64f86e0501392346f46a71fea
Jude Rodriguez <rodriguezzude@gmail.com>
2026-09-19 23:34:33 +0530

    env vars added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA9Ay9KOGv-45mg8CG42FJcjcmuMibSwlA",
+    authDomain: "portfolio-zvdas.firebaseapp.com",
+    projectId: "portfolio-zvdas",
+    storageBucket: "portfolio-zvdas.firebasestorage.app",
+    messagingSenderId: "747441241828",
+    appId: "1:747441241828:web:0db5f1d8224e71a648b6f1",
+    measurementId: "G-VGPYEZH1C2"
+  }
 };
 
 /*
